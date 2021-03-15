65026d8d2bd4820619b63421f0138cdaf3d37197
franzi-r <63734276+franzi-r@users.noreply.github.com>
2021-03-15 10:48:39 +0100

    Update Japan_Regression_2.R

--- Japan_Regression_2.R
+++ Japan_Regression_2.R
@@ -29,13 +29,34 @@ reg_blind <- rq(triggeredMag ~ triggeringMag, tau = q10, data = japan_reg_blind)
 
 plot(summary(reg_blind))
 
+q100 <- seq(from = 0, to = 1, by = 0.01)
+reg_blind100 <- rq(triggeredMag ~ triggeringMag, tau = q100, data = japan_reg_blind)
+plot(summary(reg_blind100))
 ## regressoin 100 quantile
-
-q100 <- seq(0.01, 0.99, by = 0.01)
-
 model_3 <- rq(triggeredMag ~ triggeringMag, tau = q100, data = japan_reg )
 
 plot(summary(model_3))
+
+### t als Einflussvariable
+## Zeitdifferenz zwischen triggerenden und getriggerten Beben
+japan_reg <- mutate(japan_reg, timediff = 0)
+for(i in 1:length(japan_reg$eventID)){
+   triggering <- japan_reg$triggeredFrom[i] 
+   ttrigg <- japan_reg$t[triggering]
+   japan_reg$timediff[i] <- japan_reg$t[i] - ttrigg
+}
+## Modelle
+modelt_1 <- rq(triggeredMag ~ triggeringMag + timediff, data = japan_reg)
+modelt_01 <- rq(triggeredMag ~ triggeringMag + timediff, tau = q10, data = japan_reg)
+modelt_01
+plot(summary(modelt_01))
+
+modelt_001 <- rq(triggeredMag ~ triggeringMag + timediff, tau = q100, data = japan_reg)
+modelt_001
+plot(summary(modelt_001))
+
+### Wald Test, ob timediff signifikanten Einfluss hat
+anova(modelt_1, model_1,  test ="Wald")
 
 ## Daten nach aufsteigender triggernder magnitude ordnen
 
@@ -55,4 +76,3 @@ ggplot(japan_quantil_1, aes(triggeringMag, triggeredMag))+
 ggplot(japan_quantil_4, aes(triggeringMag, triggeredMag))+
   geom_point()+
   theme_bw()
-
